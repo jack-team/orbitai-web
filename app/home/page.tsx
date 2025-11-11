6cbcb3674d6d94ba78d0b50e7574ce3aadbae1a6
Jack Jiang <578091306@qq.com>
2025-11-11 21:08:09 +0800

    feat: 首页样式开发

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,6 +3,8 @@ import BaseLayout from '@/components/BaseLayout';
 import Banner from './components/Banner';
 import Block1 from './components/Block1';
 import Block2 from './components/Block2';
+import Block3 from './components/Block3';
+import Block4 from './components/Block4';
 
 const Home: FC = () => {
   return (
@@ -10,6 +12,8 @@ const Home: FC = () => {
       <Banner />
       <Block1 />
       <Block2 />
+      <Block3 />
+      <Block4 />
     </BaseLayout>
   );
 }
